7567e24c8259f16d95c912b25d6cf4ab4b57d476
Devin Samarin <me@dsamar.in>
2018-05-22 20:26:39 -0700

    Include bootstrap collapse

--- ts/zerodrop.ts
+++ ts/zerodrop.ts
@@ -2,6 +2,7 @@
 import * as $ from 'jquery';
 import 'bootstrap/js/src/util';
 import 'bootstrap/js/src/tab';
+import 'bootstrap/js/src/collapse';
 import 'bootstrap/js/src/dropdown';
 
 function humanFileSize(bytes: number, si: boolean): string {
